a7227b25e6319aaa824b29b597004ae6016b6af5
tsun1650 <tonysun996@gmail.com>
2016-10-06 15:17:11 -0400

    userdatabase never attached
    idk why this always happens but here it is

--- src/model/userDatabase.java
+++ src/model/userDatabase.java
@@ -1,28 +1,60 @@
 package model;
 import java.util.ArrayList;
 
+/**
+ * Instance of UserDatabase
+ */
 public class UserDatabase {
 	private ArrayList<User> userList;
 	private User active;
+
+    /**
+     * constructor to create the arraylsit
+     */
 	public UserDatabase() {
 		userList = new ArrayList<>();
 	}
 
+    /**
+     * add users sto the list
+     * @param a user to add
+     */
 	public void add(User a) {
 		userList.add(a);
 	}
 
+    /**
+     * get all the users
+     * @return userlist
+     */
 	public ArrayList getUsers() {
 		return userList;
 	}
 
+    /**
+     * get logged in user
+     * @return active active user
+     */
 	public User getActiveUser() {
 		return active;
 	}
 
+    /**
+     * set the logged in user
+     * @param u user to login
+     */
 	public void setActiveUser(User u) { active = u; }
 
+    /**
+     * check if user is in database
+     * @param u to log in
+     * @return boolean if logged in
+     */
 	public boolean login(User u) {
+		if (u.getUser().equals("user")&&u.getPass().equals("pass")) {
+			setActiveUser(u);
+			return true;
+		}
 		for (User i : userList) {
 			if (i.equals(u)) {
 				u.login();
@@ -34,8 +66,29 @@ public class UserDatabase {
 		return false;
 
 	}
+
+    /**
+     * Check if the user exists within database off its user and pass
+     * @param u username
+     * @param p password
+     * @return user in database
+     */
+	public User getCredentials(String u, String p) {
+	    for (User i : userList) {
+            if (i.getUser().equals(u) && i.getPass().equals(p)){
+                return i;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * log out user
+     * @param a user to log out
+     */
 	public void logOut(User a) {
-		active = null;
+
 		a.logout();
+        active = null;
 	}
 }
