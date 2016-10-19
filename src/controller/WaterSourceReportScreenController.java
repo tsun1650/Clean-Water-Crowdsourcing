5df1da653d1d970ffd22e028d171160374d9fac6
David Le <davidhle721@gmail.com>
2016-10-19 08:03:55 -0400

    Update Water Source Report Screen

--- src/controller/WaterSourceReportScreenController.java
+++ src/controller/WaterSourceReportScreenController.java
@@ -1,7 +1,14 @@
 package controller;
 
+import javafx.scene.input.MouseEvent;
+
 /**
  * Created by David on 10/19/2016.
  */
 public class WaterSourceReportScreenController {
+    public void cancelClicked(MouseEvent mouseEvent) {
+    }
+
+    public void submitClicked(MouseEvent mouseEvent) {
+    }
 }
